f50b6cad211fa20a658b436b272eb2b869188e2a
MaximPlay <41975839+MaximPlay@users.noreply.github.com>
2025-12-16 11:57:22 +0500

    Update VagneraADSModDemo.py

--- VagneraADSModDemo.py
+++ VagneraADSModDemo.py
@@ -21,42 +21,24 @@ class VagneraADSMod(loader.Module):
 
     @loader.unrestricted
     async def adcmd(self, message):
-        """Установить новую ссылку по умолчанию"""
         args = utils.get_args_raw(message)
         if not args:
             await message.edit("<b>Укажите ссылку: .ad https://example.com</b>")
             return
-            
-        # Проверка корректности ссылки только при установке по умолчанию
         if not args.startswith(("http://", "https://")):
             await message.edit("<b>🚫 Ссылка должна начинаться с http:// или https://</b>")
             return
-            
-        # Устанавливаем ссылку по умолчанию
         self.default_link = args
         await message.edit(f"<b>✅ Ссылка по умолчанию обновлена: {args}</b>")
 
     @loader.unrestricted
     async def acmd(self, message):
-        """Создать кликабельную ссылку с указанным текстом"""
         args = utils.get_args_raw(message)
         reply = await message.get_reply_message()
         original_text = message.text
         if original_text.startswith(".a"):
             original_text = original_text[2:].strip()
-            
-        # Аргумент команды .a должен стать текстом ссылки
         custom_text = args
-        
-        # Использовать заранее заданную ссылку по умолчанию
         link = self.default_link
-        
-        # Генерируем HTML-ссылку
         clickable_link = f'<a href="{link}">{custom_text}</a>'
-        
-        # Если изначально не было текста, показываем только ссылку
-        if not original_text:
-            await message.edit(clickable_link, parse_mode="HTML")
-            return
-        new_text = original_text + "\n" + clickable_link
-        await message.edit(new_text, parse_mode="HTML")
+        await message.edit(clickable_link, parse_mode="HTML")
