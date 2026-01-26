34fa1d806c66799f8c8fbe78e817119bfca08b23
MaximPlay <41975839+MaximPlay@users.noreply.github.com>
2026-01-26 14:31:29 +0500

    Update DiceM1.py

--- DiceM1.py
+++ DiceM1.py
@@ -3,20 +3,16 @@ from random import randint
 from .. import loader, utils
 
 def register(cb):
-    cb(DiceModule())
+    cb(DiceModule)
 
 class DiceModule(loader.Module):
-    """Кинь кубик епта (.dice [результат])"""
-    
-    strings = {"name": "Кубик"}
+    strings = {"name": "Dice"}
 
     @loader.unrestricted
     async def dicecmd(self, message):
-        
-        
         args = utils.get_args_raw(message)
         value = int(args) if args and args.isdigit() else randint(1, 6)
-         
+        
         if value < 1 or value > 6:
             await utils.answer(message, '<b>Некорректное значение!</b>')
             return
