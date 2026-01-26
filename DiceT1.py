774a0e1bb50259697931a3d51c3f0dde4d72ed56
MaximPlay <41975839+MaximPlay@users.noreply.github.com>
2026-01-26 14:32:36 +0500

    Create DiceT1.py

---
+++ DiceT1.py
@@ -0,0 +1,21 @@
+from telethon import events
+from random import randint
+from .. import loader, utils
+
+def register(cb):
+    cb(DiceModule)
+
+class DiceModule(loader.Module):
+    strings = {"name": "Dice"}
+
+    @loader.unrestricted
+    async def dicecmd(self, message):
+        args = utils.get_args_raw(message)
+        value = int(args) if args and args.isdigit() else randint(1, 6)
+        
+        if value < 1 or value > 6:
+            await utils.answer(message, '<b>Некорректное значение!</b>')
+            return
+        
+        await self.client.send_dice(message.to_id, emoji="🎲", value=value)
+        await message.delete()
