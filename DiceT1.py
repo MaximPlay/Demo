fd16c37e236dbac2f480aad23d7f9e1e6087c5fb
MaximPlay <41975839+MaximPlay@users.noreply.github.com>
2026-01-26 14:35:19 +0500

    Update DiceT1.py

--- DiceT1.py
+++ DiceT1.py
@@ -3,19 +3,24 @@ from random import randint
 from .. import loader, utils
 
 def register(cb):
-    cb(DiceModule)
+    cb(DiceModule())
 
 class DiceModule(loader.Module):
+    """Модуль для броска игральной кости"""
     strings = {"name": "Dice"}
 
     @loader.unrestricted
     async def dicecmd(self, message):
+        """Бросает игральную кость. Опционально можно указать нужный результат.
+           Пример: '.dice 3' — бросит кубик с результатом 3,
+                   '.dice' — бросит случайный результат от 1 до 6."""
+        
         args = utils.get_args_raw(message)
         value = int(args) if args and args.isdigit() else randint(1, 6)
         
         if value < 1 or value > 6:
-            await utils.answer(message, '<b>Некорректное значение!</b>')
+            await utils.answer(message, '<b>Неверное значение. Число должно быть от 1 до 6.</b>')
             return
         
-        await self.client.send_dice(message.to_id, emoji="🎲", value=value)
+        await self.client.send_dice(message.to_id, emoji='🎲', value=value)
         await message.delete()
