0fa863b512c02dd78d76e0425d00b449d9fbf51d
ItsGenitz <41975839+MaximPlay@users.noreply.github.com>
2026-07-27 10:19:13 +0500

    Update and rename DiceM1.py to DiceM21.py

--- DiceM21.py
+++ DiceM21.py
@@ -2,23 +2,26 @@ from telethon import events
 from random import randint
 from .. import loader, utils
 
-def register(cb):
-    cb(DiceModule())
-
-class DiceModule(loader.Module):
+@loader.module(name="Dice", author="MaximPlay")
+class DiceMod(loader.Module):
     strings = {"name": "Dice"}
 
-    @loader.unrestricted
+    @loader.command()
     async def dicecmd(self, message):
+        """Бросить кости. Использование: .dice [число от 1 до 6]"""
         args = utils.get_args_raw(message)
         
         if args and args.isdigit():
             value = int(args)
             if value < 1 or value > 6:
-                await utils.answer(message, '<b>Некорректное значение! Допустимы только числа от 1 до 6.</b>')
+                await utils.answer(message, '<b>❌ Некорректное значение! Допустимы только числа от 1 до 6.</b>')
                 return
         else:
             value = randint(1, 6)
         
         await self.client.send_dice(message.to_id, emoji="🎲", value=value)
         await message.delete()
+
+def register(module_name):
+    """Регистрация модуля в системе"""
+    return DiceMod()
