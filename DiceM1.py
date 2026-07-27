c7e11fee59b34409f7b09fb73feafef17bb7a1db
ItsGenitz <41975839+MaximPlay@users.noreply.github.com>
2026-07-27 10:16:57 +0500

    Update DiceM1.py

--- DiceM1.py
+++ DiceM1.py
@@ -3,7 +3,7 @@ from random import randint
 from .. import loader, utils
 
 def register(cb):
-    cb(DiceModule)
+    cb(DiceModule())
 
 class DiceModule(loader.Module):
     strings = {"name": "Dice"}
@@ -11,11 +11,14 @@ class DiceModule(loader.Module):
     @loader.unrestricted
     async def dicecmd(self, message):
         args = utils.get_args_raw(message)
-        value = int(args) if args and args.isdigit() else randint(1, 6)
         
-        if value < 1 or value > 6:
-            await utils.answer(message, '<b>Некорректное значение!</b>')
-            return
+        if args and args.isdigit():
+            value = int(args)
+            if value < 1 or value > 6:
+                await utils.answer(message, '<b>Некорректное значение! Допустимы только числа от 1 до 6.</b>')
+                return
+        else:
+            value = randint(1, 6)
         
         await self.client.send_dice(message.to_id, emoji="🎲", value=value)
         await message.delete()
