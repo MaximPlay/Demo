856ca00096860245f6f945030e1fa5541a8d146d
ItsGenitz <41975839+MaximPlay@users.noreply.github.com>
2026-07-27 10:39:52 +0500

    Update DiceM21.py

--- DiceM21.py
+++ DiceM21.py
@@ -1,32 +1,87 @@
-# -*- coding: utf-8 -*-
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
 
-# Module author: @MaximPlay
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 
-from random import randint
-from .. import loader, utils
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import asyncio
+import logging
+
+from telethon.tl.types import InputMediaDice
+
+from .. import loader, utils, security
+
+logger = logging.getLogger(__name__)
 
 
 @loader.tds
-class DiceModule(loader.Module):
-    """Бросок игровых костей"""
-    
+class DiceMod(loader.Module):
+    """Dice"""
     strings = {"name": "Dice"}
+
+    def __init__(self):
+        self.config = loader.ModuleConfig("POSSIBLE_VALUES", {"": [1, 2, 3, 4, 5, 6],
+                                                              "🎲": [1, 2, 3, 4, 5, 6],
+                                                              "🎯": [1, 2, 3, 4, 5, 6],
+                                                              "🏀": [1, 2, 3, 4, 5]},
+                                          "Mapping of emoji to possible values")
 
     @loader.unrestricted
     async def dicecmd(self, message):
-        """Бросить кости. Использование: .dice [число от 1 до 6]"""
-        args = utils.get_args_raw(message)
-        
-        if args and args.isdigit():
-            value = int(args)
-            if value < 1 or value > 6:
-                await utils.answer(message, '<b>❌ Некорректное значение! Допустимы только числа от 1 до 6.</b>')
-                return
+        """Rolls a die (optionally with the specified value)
+           .dice <emoji> <outcomes> <count>"""
+        args = utils.get_args(message)
+        if await self.allmodules.check_security(message, security.OWNER | security.SUDO):
+            try:
+                emoji = args[0]
+            except IndexError:
+                emoji = "🎲"
+            possible = self.config["POSSIBLE_VALUES"].get(emoji, None)
+            if possible is None:
+                emoji = "🎲"
+                possible = self.config["POSSIBLE_VALUES"][emoji]
+            values = set()
+            try:
+                for val in args[1].split(","):
+                    value = int(val)
+                    if value in possible:
+                        values.add(value)
+            except (ValueError, IndexError):
+                values.clear()
+            try:
+                count = int(args[2])
+            except (ValueError, IndexError):
+                count = 1
+            rolled = -1
+            done = 0
+            chat = message.to_id
+            client = message.client
+            while True:
+                task = client.send_message(chat, file=InputMediaDice(emoji))
+                if message:
+                    message = (await asyncio.gather(message.delete(), task))[1]
+                else:
+                    message = await task
+                rolled = message.media.value
+                logger.debug("Rolled %d", rolled)
+                if rolled in values or not values:
+                    done += 1
+                    message = None
+                    if done == count:
+                        break
         else:
-            value = randint(1, 6)
-        
-        try:
-            await self.client.send_dice(message.to_id, emoji="🎲", value=value)
-            await message.delete()
-        except Exception as e:
-            await utils.answer(message, f'<b>❌ Ошибка: {str(e)}</b>')
+            try:
+                emoji = args[0]
+            except IndexError:
+                emoji = "🎲"
+            await message.reply(file=InputMediaDice(emoji))
